76a46f032a3ad085e6f38fd093627c39bf29a420
onion-eater <nolanjiang88@gmail.com>
2025-11-29 08:40:38 -0800

    project

--- src/app/ProjectsPage.tsx
+++ src/app/ProjectsPage.tsx
@@ -49,6 +49,14 @@ export default function ProjectsPage() {
             github=""
             livelink=""
           />
+          <Project
+            isDark={isDark}
+            title="Koala"
+            description="Chrome extension using React and Chrome API that replaces distracting websites with AI-powered learning (e.g. flashcards and quizzes) with Gemini API. Live leaderboard built with Supabase, and responsive UI built with Tailwind CSS."
+            stack={["React", "JavaScript", "Tailwind CSS", "Chrome API", "Gemini API", "Supabase", "Git"]}
+            github="https://github.com/tkl0000/koala"
+            livelink=""
+          />
         </div>
       </div>
     </div>
