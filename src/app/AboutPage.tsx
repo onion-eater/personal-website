6bc5f150325d4c23329784425da49c6f08457dfe
onion-eater <nolanjiang88@gmail.com>
2026-03-13 02:18:40 -0400

    better

--- src/app/AboutPage.tsx
+++ src/app/AboutPage.tsx
@@ -10,15 +10,8 @@ export default function AboutPage() {
       <div className="min-h-screen flex flex-col items-center justify-center">
         <Navbar isDark={isDark} onToggle={() => setIsDark((prev) => !prev)} />
         <h1>
-          My name is <u>Nolan Jiang</u>. I am studying <u>Mathematics</u> at <u>Carnegie Mellon
-          University</u>.
+          My name is <u>Nolan Jiang</u>.
         </h1>
-        <br></br>
-        <h2>Find Me Here:</h2>
-        <p><a href="https://www.linkedin.com/in/nolan-jiang" className="underline hover:text-blue-500" target="_blank">Linkedin</a> |{" "}
-        <a href="https://www.github.com/onion-eater" className="underline hover:text-blue-500" target="_blank">GitHub</a> |{" "}
-        <a href="mailto:nolanjiang@cmu.edu" className="underline hover:text-blue-500" target="_blank">Email</a> |{" "}
-        <a href="https://www.instagram.com/jiang_nolan/" className="underline hover:text-blue-500" target="_blank">Instagram</a></p>
 
 
       </div>
